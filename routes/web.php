51a9476f9f31308499dc5126089e8095816a8db4
Achmad Mac <eririfky29@gmail.com>
2021-08-12 21:23:11 +0700

    Add route to author username and edit paramater from user to author

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
+use App\Models\User;
 
 
 /*
@@ -38,5 +39,12 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name,
+    ]);
+});
+
+Route::get('/author/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts,
     ]);
 });
