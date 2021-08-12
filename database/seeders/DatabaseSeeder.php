6ad6a43ef467ea2b5e911796684e11bdd066d666
Achmad Mac <eririfky29@gmail.com>
2021-08-12 21:17:34 +0700

    Create User, Category, and Post factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::create([
-            'name' => 'Achmad Rifki',
-            'email' => 'eririfky29@gmail.com',
-            'password' => bcrypt('12345678')
-        ]);
 
-        User::create([
-            'name' => 'Rava Akhtareez',
-            'email' => 'rava@gmail.com',
-            'password' => bcrypt('54321')
-        ]);
+        // User::create([
+        //     'name' => 'Achmad Rifki',
+        //     'email' => 'eririfky29@gmail.com',
+        //     'password' => bcrypt('12345678')
+        // ]);
+
+        // User::create([
+        //     'name' => 'Rava Akhtareez',
+        //     'email' => 'rava@gmail.com',
+        //     'password' => bcrypt('54321')
+        // ]);
 
         Category::create([
             'name' => 'Programming Language',
@@ -45,22 +46,23 @@ class DatabaseSeeder extends Seeder
             'slug' => 'science'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit!',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit! Minus distinctio ad ullam quis dicta sint, assumenda fugit nisi natus maiores ex deserunt facere, ratione iste corrupti sit est omnis asperiores eos? Nisi quis, aperiam officiis dolor ipsum corporis aut, aspernatur eum cum at inventore atque quisquam esse sequi, accusamus reiciendis quasi quaerat voluptatum cumque vitae velit sunt! Veritatis, provident suscipit consectetur natus eaque adipisci sint, ut, mollitia neque rem magni ab maiores quo. Voluptatum vero a maiores harum dicta provident illum? Aliquid doloremque dolores rem repellendus?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(10)->create();
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit!',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit! Minus distinctio ad ullam quis dicta sint, assumenda fugit nisi natus maiores ex deserunt facere, ratione iste corrupti sit est omnis asperiores eos? Nisi quis, aperiam officiis dolor ipsum corporis aut, aspernatur eum cum at inventore atque quisquam esse sequi, accusamus reiciendis quasi quaerat voluptatum cumque vitae velit sunt! Veritatis, provident suscipit consectetur natus eaque adipisci sint, ut, mollitia neque rem magni ab maiores quo. Voluptatum vero a maiores harum dicta provident illum? Aliquid doloremque dolores rem repellendus?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit!',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit! Minus distinctio ad ullam quis dicta sint, assumenda fugit nisi natus maiores ex deserunt facere, ratione iste corrupti sit est omnis asperiores eos? Nisi quis, aperiam officiis dolor ipsum corporis aut, aspernatur eum cum at inventore atque quisquam esse sequi, accusamus reiciendis quasi quaerat voluptatum cumque vitae velit sunt! Veritatis, provident suscipit consectetur natus eaque adipisci sint, ut, mollitia neque rem magni ab maiores quo. Voluptatum vero a maiores harum dicta provident illum? Aliquid doloremque dolores rem repellendus?',
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit!',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quae velit nemo libero dolor voluptate debitis iure minus. Quam tempora dolor fugit! Minus distinctio ad ullam quis dicta sint, assumenda fugit nisi natus maiores ex deserunt facere, ratione iste corrupti sit est omnis asperiores eos? Nisi quis, aperiam officiis dolor ipsum corporis aut, aspernatur eum cum at inventore atque quisquam esse sequi, accusamus reiciendis quasi quaerat voluptatum cumque vitae velit sunt! Veritatis, provident suscipit consectetur natus eaque adipisci sint, ut, mollitia neque rem magni ab maiores quo. Voluptatum vero a maiores harum dicta provident illum? Aliquid doloremque dolores rem repellendus?',
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
     }
 }
